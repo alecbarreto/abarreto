a6f381cde0f7f11b01dfdd7363e8d67c86fce556
Alec Barreto <abarreto@standrews-de.org>
2017-06-15 22:55:40 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,6 @@
 def FizzBuzz():
     for num in range(1,101):
-        if num % 3 == 0 and num % 5 == 0:
+        if num % 15 == 0:
             print  str(num) + " is divisible by 3 and 5!"
         elif num % 3 == 0:
             print str(num) + " is divisible by 3!"
